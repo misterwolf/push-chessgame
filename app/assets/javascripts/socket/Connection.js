df6b8f77405b1a6a7eaadff5b284b340ca0a59da
misterwolf <dario.gas@gmail.com>
2016-09-18 22:42:09 +0200

    first two callbacks unit test for connection, it will change

--- app/assets/javascripts/socket/Connection.js
+++ app/assets/javascripts/socket/Connection.js
@@ -1,44 +1,69 @@
 //= require websocket_rails/main
 //= require namespace
 //= require socket/namespace
+//= require lib/dom
 
-(function(connection){
+(function(connection, dom){
   'use strict';
+
+  // TO DO:
+  // 1 IN FUTURE DELETE THIS MESSAGE
+  // 2 MOVE ALL THE HTML REFERENCES IN THIS FILE IN ANOTHER SEPARATE ONE.
+  // 2.1 LIKE: CONSTANTS AND DOM.ID|CLASSES!
+  // 2.2 IT COULD BE A GOOD IDEA AND THIS IMPLIES TO STUB OBJECTS IN TEST SUITE! 
+  var DIV_ID_WELCOME_USER = 'welcome_user';
+  var DIV_ID_GOODBYE_USER = 'goodbye_user';
+  var MESSAGE_FOR_WELCOME = 'Welcome user! :)';
+  var MESSAGE_FOR_GOODBYE = 'Goodbye user! :)';
+  // ----
 
   connection.dispatcher = null;
   connection.opened = false;
+  connection.channel_new_client_connected = null;
+  connection.channel_get_all_clients = null;
 
-  connection.init = function(params, cb){
+  connection.init = function(params){
     params = params || {};
     if (typeof params.url !== 'string' && params.url === '') { return;}
     this.dispatcher = new WebSocketRails(params.url);
     var channel = this.dispatcher.subscribe('new_client_connected');
 
-    // var channel = dispatcher.subscribe('new_client_connected');
     connection.dispatcher.on_open = onOpen;
     connection.dispatcher.connection_closed = onClose;
     connection.dispatcher.connection_error = onError;
 
-    channel.bind('new_client_connected', function(data) {
+    connection.channel_new_client_connected = connection.dispatcher.subscribe('new_client_connected');
+    connection.channel_get_all_clients = connection.dispatcher.subscribe('get_all_clients');
+
+    connection.channel_new_client_connected.bind('new_client_connected', function(data) {
       console.log('channel event received: ' + data);
     });
 
-    if (cb){
-      cb();
-    }
+    connection.channel_get_all_clients.bind('get_all_clients', function(data) {
+      console.log('channel event received: ' + data);
+    });
+
+  };
+
+  connection.disconnect = function(data){
+    connection.dispatcher.connection_closed();
+    console.log('hello');
   };
 
   var onOpen = function(data){
-    // TO DO: do we have to alert user for init connection?
+    dom.id(DIV_ID_WELCOME_USER).innerHTML = MESSAGE_FOR_WELCOME;
   };
 
   var onClose = function(){
-    // TO DO:
-     console.log('HELLO')
+    dom.id(DIV_ID_GOODBYE_USER).innerHTML = MESSAGE_FOR_GOODBYE;
   };
 
   var onError = function(data){
     console.log('on error' + data);
+  };
+
+  connection.channel = function(){
+
   };
 
   connection.send = function(){
@@ -49,4 +74,4 @@
     // TO DO:
   };
 
-})(window._chess.socket.connection = {});
+})(window._chess.socket.connection = {}, window._chess.lib.dom);
--- spec/javascripts/socket/SpecConnectionSpec.js
+++ spec/javascripts/socket/SpecConnectionSpec.js
@@ -1,9 +1,9 @@
 //= require socket/Connection.js
 
-// take a look at this for new idea or inspiration...
-// https://github.com/websocket-rails/websocket-rails/blob/master/spec/javascripts/websocket_rails/event_spec.coffee
+jasmine.getFixtures().fixturesPath = 'spec/javascripts/fixtures/';
 
-(function(connection){
+(function(connection, dom){
+
   describe('Socket', function() {
     var stub = {
       url: 'localhost:3001/websocket'
@@ -18,34 +18,67 @@
         it('have a connection url', function() {
           expect(connection.dispatcher.url).toBe(stub.url);
         });
+        // USELESS
+        // describe('dispatcher', function(){
+        //   it('to be defined', function() {
+        //     expect(connection.dispatcher).toBeDefined();
+        //   });
+        //
+        //   it('the state should \'connecting\'', function() {
+        //     expect(connection.dispatcher.state).toBe('connecting');
+        //   });
+        //   describe('callback', function(){
+        //     it('on_open should be called', function(done) {
+        //       // it's the right way to test callbacks?
+        //       spyOn(connection.dispatcher, 'on_open');
+        //       setTimeout(function(){expect(connection.dispatcher.on_open).toHaveBeenCalled(); done();}, 200);
+        //     });
+        //     it('connection_closed should be called', function(done) {
+        //       // it's the right way to test callbacks?
+        //       spyOn(connection.dispatcher, 'connection_closed');
+        //       connection.disconnect();
+        //       setTimeout(function(){
+        //         expect(connection.dispatcher.connection_closed).nottoHaveBeenCalled();
+        //         done();
+        //       }, 200);
+        //     });
+        //   });
+        // });
+        describe('callbacks', function(){
 
-        describe('dispatcher', function(){
-          it('to be defined', function() {
-            expect(connection.dispatcher).toBeDefined();
+          beforeEach(function(){
+            loadFixtures('connection/elements.html');
           });
 
-          it('the state should \'connecting\'', function() {
-            expect(connection.dispatcher.state).toBe('connecting');
-          });
-          describe('callback', function(){
-            it('on_open should be called', function(done) {
-              spyOn(connection.dispatcher, 'on_open');
-              setTimeout(function(){expect(connection.dispatcher.on_open).toHaveBeenCalled();done();}, 150);
+          describe('onOpen', function(){
+            it('welcome message is set', function(done) {
+              setTimeout(function(){
+                // toContain is more generic: a day i will decide to insert into 'welcome_user' div some other elements.
+                expect( $('#welcome_user').html() ).toContain('Welcome user! :)');
+                done();
+              }, 200);
             });
-
-            it('connection_closed should be called', function(/*done*/) {
-              connection.dispatcher.disconnect();
-              spyOn(connection.dispatcher, 'connection_closed');
-              // setTimeout(function(){
-              expect(connection.dispatcher.state).toBe('disconnected');
-              expect(connection.dispatcher.connection_closed).toHaveBeenCalled();
-              // done();}, 3010);
-            });
-
           });
+          describe('onClose', function(){
+            it('goodbye message is set', function(done) {
+              connection.disconnect();
+              setTimeout(function(){
+                // toContain is more generic: a day i will decide to insert into 'welcome_user' div some other elements.
+                expect( $('#goodbye_user').html() ).not.toContain('Goodbye user! :)');
+                done();
+              }, 200);
+            });
+            // it('all clients container emptied', function() {
+            // });
+          });
+          // describe('onReceiveMessage', function(){
+          //   it('remove element from all users', function() {
+          //   });
+          // });
         });
+
       });
 
     });
   });
-})(window._chess.socket.connection);
+})(window._chess.socket.connection, window._chess.lib.dom );
